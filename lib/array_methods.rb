74b2ae29f7df5329f37618edc402ebde8ba25cf9
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-11 02:02:33 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,11 +3,11 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-
+  sorted_array = array.sort
 end
 
 def using_reverse(array)
-
+    reversed_array = array.reverse
 end
 
 def using_first(array)
